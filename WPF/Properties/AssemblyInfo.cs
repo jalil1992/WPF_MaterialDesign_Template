91b03714093e14c2dc07c401fdfe70c918c20d1a
Jalil <jalilmarkel1992@gmail.com>
2019-01-27 13:49:18 -0500

    Props change  AssemblyInfo

--- WPF/Properties/AssemblyInfo.cs
+++ WPF/Properties/AssemblyInfo.cs
@@ -1,5 +1,5 @@
 //      Build Number
-using System.Resources;
+//[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Windows;
@@ -15,24 +15,24 @@ using System.Windows;
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-// by using the '*' as shown below:
+[assembly: AssemblyCulture("")]
 // Setting ComVisible to false makes the types in this assembly not visible
 [assembly: AssemblyProduct("WpfApp1")]
 // COM, set the ComVisible attribute to true on that type.
-//
-
-//      Revision
+using System.Resources;
+//[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
+                                              //(used if a resource is not found in the page,
 // You can specify all the values or you can default the Build and Revision Numbers
 )]
-//in your source files, set the <UICulture> to en-US.  Then uncomment
-//the NeutralResourceLanguage attribute below.  Update the "en-US" in
+
+// by using the '*' as shown below:
 using System.Runtime.InteropServices;
 
 //[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
 
 
 [assembly: ThemeInfo(
-// by using the '*' as shown below:
+                                              // app, or any theme specific resource dictionaries)
                                      //(used if a resource is not found in the page,
                                      // or application resource dictionaries)
     ResourceDictionaryLocation.SourceAssembly //where the generic resource dictionary is located
@@ -43,14 +43,14 @@ using System.Runtime.InteropServices;
 
 // Version information for an assembly consists of the following four values:
 //
-//      Major Version
-//in your source files, set the <UICulture> to en-US.  Then uncomment
+//
+//      Build Number
 //      Build Number
 //      Revision
 //
-
-// by using the '*' as shown below:
-//      Revision
+//[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
+//      Build Number
+)]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: log4net.Config.XmlConfigurator(ConfigFile = "log4net.config")]
+//      Build Number
