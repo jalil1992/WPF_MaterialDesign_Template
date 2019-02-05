3c741981fa15f51d71e7e44ec88e21e6f98a41af
Jalil <jalilmarkel1992@gmail.com>
2019-02-05 13:34:35 -0500

    User manage  AssemblyInfo

--- WPF/Properties/AssemblyInfo.cs
+++ WPF/Properties/AssemblyInfo.cs
@@ -1,9 +1,9 @@
-//      Build Number
+[assembly: AssemblyTitle("WpfApp1")]
 //[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
-using System.Runtime.CompilerServices;
+// General Information about an assembly is controlled through the following
 using System.Runtime.InteropServices;
-using System.Windows;
-
+// COM, set the ComVisible attribute to true on that type.
+//[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 //      Build Number
@@ -12,28 +12,28 @@ using System.Windows;
 //[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
 using System.Windows;
 [assembly: AssemblyProduct("WpfApp1")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
-[assembly: AssemblyTrademark("")]
+
+using System.Windows;
 [assembly: AssemblyCulture("")]
 [assembly: AssemblyCulture("")]
-// Setting ComVisible to false makes the types in this assembly not visible
+
 [assembly: AssemblyProduct("WpfApp1")]
-// COM, set the ComVisible attribute to true on that type.
-using System.Resources;
+using System.Windows;
+[assembly: AssemblyProduct("WpfApp1")]
 //[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
-                                              //(used if a resource is not found in the page,
-// You can specify all the values or you can default the Build and Revision Numbers
+
+[assembly: AssemblyTrademark("")]
 )]
 
 // by using the '*' as shown below:
 using System.Runtime.InteropServices;
 
+using System.Runtime.InteropServices;
+
 //[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
-
-
 [assembly: ThemeInfo(
                                               // app, or any theme specific resource dictionaries)
-                                     //(used if a resource is not found in the page,
+[assembly: AssemblyCulture("")]
                                      // or application resource dictionaries)
     ResourceDictionaryLocation.SourceAssembly //where the generic resource dictionary is located
                                               //(used if a resource is not found in the page,
@@ -42,15 +42,15 @@ using System.Runtime.InteropServices;
 
 
 // Version information for an assembly consists of the following four values:
-//
+[assembly: AssemblyProduct("WpfApp1")]
 //
 //      Build Number
-//      Build Number
+
 //      Revision
 //
 //[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
 //      Build Number
 )]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+//[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
 //      Build Number
