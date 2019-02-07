3cbfc5e0ed45839805f9da9e4bad32f2f7fdd667
Jalil <jalilmarkel1992@gmail.com>
2019-02-07 12:36:42 -0500

    Props change  MainWindow

--- WPF/MainWindow.xaml.cs
+++ WPF/MainWindow.xaml.cs
@@ -1,28 +1,28 @@
-using System.Windows.Documents;
-using System.Collections.Generic;
-using System.Collections.Generic;
-using System.Text;
-    }
 using System.Windows;
+        public MainWindow()
+using System.Windows.Media;
+using System.Text;
+using System.Windows;
+        public MainWindow()
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows;
 using System.Text;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-        public MainWindow()
-using System.Windows.Shapes;
-        {
+using System.Windows.Data;
+using System.Text;
+    }
+using System.Windows;
+using System.Text;
 namespace WpfApp1
-{
+    /// </summary>
     /// <summary>
-        public MainWindow()
+    }
     /// </summary>
     }
 using System.Threading.Tasks;
-        public MainWindow()
-        {
+using System.Windows;
+using System.Text;
 using System.Collections.Generic;
         public MainWindow()
 using System.Text;
-        public MainWindow()
+using System.Windows.Controls;
