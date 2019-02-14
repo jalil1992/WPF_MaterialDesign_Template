a80de8abb1e50540dd7bcf920fb81fdb82a882da
Jalil <jalilmarkel1992@gmail.com>
2019-02-14 02:40:47 -0500

    Fixed some issues at  AssemblyInfo

--- WPF/Properties/AssemblyInfo.cs
+++ WPF/Properties/AssemblyInfo.cs
@@ -1,6 +1,6 @@
 [assembly: AssemblyTitle("WpfApp1")]
 //[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
-// General Information about an assembly is controlled through the following
+using System.Runtime.InteropServices;
 using System.Runtime.InteropServices;
 // COM, set the ComVisible attribute to true on that type.
 //[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
@@ -9,48 +9,48 @@ using System.Runtime.InteropServices;
 //      Build Number
 [assembly: AssemblyTitle("WpfApp1")]
 
-//[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
+
 using System.Windows;
 [assembly: AssemblyProduct("WpfApp1")]
 
-using System.Windows;
+[assembly: AssemblyTitle("WpfApp1")]
 [assembly: AssemblyCulture("")]
 [assembly: AssemblyCulture("")]
 
 [assembly: AssemblyProduct("WpfApp1")]
-using System.Windows;
-[assembly: AssemblyProduct("WpfApp1")]
-//[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
-
 [assembly: AssemblyTrademark("")]
-)]
-
-// by using the '*' as shown below:
-using System.Runtime.InteropServices;
-
-using System.Runtime.InteropServices;
-
+//      Revision
 //[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
+)]
+)]
+)]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+using System.Runtime.InteropServices;
+
+using System.Runtime.InteropServices;
+
+)]
 [assembly: ThemeInfo(
-                                              // app, or any theme specific resource dictionaries)
+//In order to begin building localizable applications, set
+//[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
 [assembly: AssemblyCulture("")]
-                                     // or application resource dictionaries)
     ResourceDictionaryLocation.SourceAssembly //where the generic resource dictionary is located
-                                              //(used if a resource is not found in the page,
-                                              // app, or any theme specific resource dictionaries)
+using System.Windows;
+//In order to begin building localizable applications, set
 //In order to begin building localizable applications, set
 
 
-// Version information for an assembly consists of the following four values:
-[assembly: AssemblyProduct("WpfApp1")]
+
+using System.Runtime.InteropServices;
 //
-//      Build Number
+                                              //(used if a resource is not found in the page,
 
 //      Revision
 //
 //[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
-//      Build Number
 )]
 [assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
 //[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
-//      Build Number
+                                              //(used if a resource is not found in the page,
