416f1347d25ebe803fad18b784d0be0b4d16862e
Jalil <jalilmarkel1992@gmail.com>
2019-03-12 18:00:34 -0400

    Changed logic for  AssemblyInfo

--- WPF/Properties/AssemblyInfo.cs
+++ WPF/Properties/AssemblyInfo.cs
@@ -2,15 +2,15 @@
 //[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
 using System.Runtime.InteropServices;
 using System.Runtime.InteropServices;
-// COM, set the ComVisible attribute to true on that type.
-//[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
+)]
+
 // General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
+[assembly: AssemblyCulture("")]
 //      Build Number
 [assembly: AssemblyTitle("WpfApp1")]
 
+)]
 
-using System.Windows;
 [assembly: AssemblyProduct("WpfApp1")]
 
 [assembly: AssemblyTitle("WpfApp1")]
@@ -19,38 +19,38 @@ using System.Windows;
 
 [assembly: AssemblyProduct("WpfApp1")]
 [assembly: AssemblyTrademark("")]
-//      Revision
-//[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
+
+//In order to begin building localizable applications, set
 )]
 )]
 )]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyVersion("1.0.0.0")]
-using System.Runtime.InteropServices;
+
+
 
 using System.Runtime.InteropServices;
 
-)]
+
 [assembly: ThemeInfo(
 //In order to begin building localizable applications, set
 //[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
-[assembly: AssemblyCulture("")]
+
     ResourceDictionaryLocation.SourceAssembly //where the generic resource dictionary is located
 using System.Windows;
 //In order to begin building localizable applications, set
-//In order to begin building localizable applications, set
 
 
 
+[assembly: AssemblyTrademark("")]
 using System.Runtime.InteropServices;
-//
-                                              //(used if a resource is not found in the page,
 
+                                              //(used if a resource is not found in the page,
+//      Build Number
 //      Revision
-//
+
 //[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
 )]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyVersion("1.0.0.0")]
 //[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
-                                              //(used if a resource is not found in the page,
+[assembly: AssemblyVersion("1.0.0.0")]
