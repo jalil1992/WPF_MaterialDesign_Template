5a24f57865172e1695326a91c9e0cc86009d6a0d
Jalil <jalilmarkel1992@gmail.com>
2019-03-24 07:39:01 -0400

    Fixed some issues at  AssemblyInfo

--- WPF/Properties/AssemblyInfo.cs
+++ WPF/Properties/AssemblyInfo.cs
@@ -1,56 +1,56 @@
-[assembly: AssemblyTitle("WpfApp1")]
+
+
 //[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
 using System.Runtime.InteropServices;
-using System.Runtime.InteropServices;
-)]
 
-// General Information about an assembly is controlled through the following
+
+
 [assembly: AssemblyCulture("")]
-//      Build Number
+
 [assembly: AssemblyTitle("WpfApp1")]
 
 )]
 
-[assembly: AssemblyProduct("WpfApp1")]
 
-[assembly: AssemblyTitle("WpfApp1")]
+
+//[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
 [assembly: AssemblyCulture("")]
 [assembly: AssemblyCulture("")]
 
 [assembly: AssemblyProduct("WpfApp1")]
 [assembly: AssemblyTrademark("")]
-
+[assembly: AssemblyVersion("1.0.0.0")]
 //In order to begin building localizable applications, set
 )]
 )]
+
+
 )]
-[assembly: AssemblyVersion("1.0.0.0")]
 
-
-
+[assembly: AssemblyCulture("")]
 using System.Runtime.InteropServices;
 
-
+[assembly: AssemblyTrademark("")]
 [assembly: ThemeInfo(
 //In order to begin building localizable applications, set
+
+
+
 //[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
 
-    ResourceDictionaryLocation.SourceAssembly //where the generic resource dictionary is located
-using System.Windows;
-//In order to begin building localizable applications, set
 
 
 
-[assembly: AssemblyTrademark("")]
+
 using System.Runtime.InteropServices;
 
                                               //(used if a resource is not found in the page,
 //      Build Number
 //      Revision
 
-//[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
+
 )]
-[assembly: AssemblyVersion("1.0.0.0")]
+    ResourceDictionaryLocation.SourceAssembly //where the generic resource dictionary is located
 [assembly: AssemblyVersion("1.0.0.0")]
 //[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
 [assembly: AssemblyVersion("1.0.0.0")]
