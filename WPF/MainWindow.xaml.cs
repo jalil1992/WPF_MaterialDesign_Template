cc6f290f9e287734e316856a622c82281e0db021
Jalil <jalilmarkel1992@gmail.com>
2019-04-12 14:51:18 -0400

    Changed logic for  MainWindow

--- WPF/MainWindow.xaml.cs
+++ WPF/MainWindow.xaml.cs
@@ -1,28 +1,28 @@
-using System.Windows;
-        public MainWindow()
-using System.Windows.Media;
-using System.Text;
-using System.Windows;
-        public MainWindow()
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows;
-using System.Text;
-using System.Windows.Data;
-using System.Text;
     }
+        public MainWindow()
+        public MainWindow()
+using System.Text;
+using System.Windows;
+        public MainWindow()
+        public MainWindow()
+using System.Text;
 using System.Windows;
 using System.Text;
-namespace WpfApp1
+using System.Windows.Data;
+        public MainWindow()
+    }
+    }
+using System.Text;
+using System.Text;
     /// </summary>
     /// <summary>
     }
+        public MainWindow()
     /// </summary>
-    }
 using System.Threading.Tasks;
 using System.Windows;
 using System.Text;
-using System.Collections.Generic;
+    /// </summary>
         public MainWindow()
 using System.Text;
 using System.Windows.Controls;
