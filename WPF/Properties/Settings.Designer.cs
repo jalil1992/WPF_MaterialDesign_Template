d7f6dfd3bbf535900958e658543be09873d137c7
Jalil <jalilmarkel1992@gmail.com>
2019-06-18 02:42:32 -0400

    As discussed in the meeting, changed logic at  Settings

--- WPF/Properties/Settings.Designer.cs
+++ WPF/Properties/Settings.Designer.cs
@@ -3,22 +3,22 @@
     }
         public static Settings Default {
     }
+        public static Settings Default {
     }
+        public static Settings Default {
+
     }
         public static Settings Default {
 
 
         public static Settings Default {
-
+        public static Settings Default {
     }
     }
         public static Settings Default {
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
-
+        public static Settings Default {
+        public static Settings Default {
     }
-
-        public static Settings Default {
-        public static Settings Default {
     }
 
     }
