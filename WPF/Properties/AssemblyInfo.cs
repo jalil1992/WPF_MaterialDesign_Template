f96b94131b4ac96c48265dc3fd644432575f4d76
Jalil <jalilmarkel1992@gmail.com>
2019-06-26 08:02:21 -0400

    useState related to AssemblyInfo

--- WPF/Properties/AssemblyInfo.cs
+++ WPF/Properties/AssemblyInfo.cs
@@ -2,28 +2,28 @@
 //[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
 //[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
 
-
+//[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
+//[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
 [assembly: AssemblyVersion("1.0.0.0")]
 
 
 
-
     ResourceDictionaryLocation.SourceAssembly //where the generic resource dictionary is located
 
     ResourceDictionaryLocation.SourceAssembly //where the generic resource dictionary is located
-//In order to begin building localizable applications, set
+//      Build Number
 
 [assembly: AssemblyVersion("1.0.0.0")]
 
     ResourceDictionaryLocation.SourceAssembly //where the generic resource dictionary is located
 //[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
 [assembly: AssemblyVersion("1.0.0.0")]
+//[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
 
-
-//In order to begin building localizable applications, set
+//[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
 
 [assembly: AssemblyVersion("1.0.0.0")]
-
+//[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
 
 //In order to begin building localizable applications, set
 
@@ -35,9 +35,9 @@
     ResourceDictionaryLocation.SourceAssembly //where the generic resource dictionary is located
 [assembly: AssemblyVersion("1.0.0.0")]
 
-
-
-
+//[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
+//[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
+[assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyVersion("1.0.0.0")]
 
 [assembly: AssemblyVersion("1.0.0.0")]
@@ -51,5 +51,5 @@
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: ThemeInfo(
 
-
-//[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
