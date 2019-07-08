ae66f620494bc78a8b83518805309a717651af7d
Jalil <jalilmarkel1992@gmail.com>
2019-07-08 09:42:15 -0400

    Changed logic for  AssemblyInfo

--- WPF/Properties/AssemblyInfo.cs
+++ WPF/Properties/AssemblyInfo.cs
@@ -4,27 +4,27 @@
 
 //[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
 //[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
+[assembly: ThemeInfo(
+
+
+//[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
+
+
+    ResourceDictionaryLocation.SourceAssembly //where the generic resource dictionary is located
+
+
+    ResourceDictionaryLocation.SourceAssembly //where the generic resource dictionary is located
+
+    ResourceDictionaryLocation.SourceAssembly //where the generic resource dictionary is located
 [assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+//[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
 
+//[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
 
-
-    ResourceDictionaryLocation.SourceAssembly //where the generic resource dictionary is located
-
-    ResourceDictionaryLocation.SourceAssembly //where the generic resource dictionary is located
+[assembly: AssemblyVersion("1.0.0.0")]
+//[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
 //      Build Number
-
-[assembly: AssemblyVersion("1.0.0.0")]
-
-    ResourceDictionaryLocation.SourceAssembly //where the generic resource dictionary is located
-//[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
-[assembly: AssemblyVersion("1.0.0.0")]
-//[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
-
-//[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
-
-[assembly: AssemblyVersion("1.0.0.0")]
-//[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
-
 //In order to begin building localizable applications, set
 
 
@@ -38,16 +38,16 @@
 //[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
 //[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyVersion("1.0.0.0")]
+
 
 [assembly: AssemblyVersion("1.0.0.0")]
 //In order to begin building localizable applications, set
-[assembly: AssemblyVersion("1.0.0.0")]
+
 //[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
 
 //      Build Number
 //[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
-//      Build Number
+
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: ThemeInfo(
 
