78d1908de56c0e94b38b9d1b23b48a1457924e20
Jalil <jalilmarkel1992@gmail.com>
2019-09-05 14:19:52 -0400

    Changed logic for  AssemblyInfo

--- WPF/Properties/AssemblyInfo.cs
+++ WPF/Properties/AssemblyInfo.cs
@@ -1,14 +1,14 @@
 //[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
 
 
+
+[assembly: ThemeInfo(
+//[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
+[assembly: ThemeInfo(
+
+
+//[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: ThemeInfo(
-//[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
-[assembly: ThemeInfo(
-
-
-//[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
-
 
     ResourceDictionaryLocation.SourceAssembly //where the generic resource dictionary is located
 
@@ -27,7 +27,7 @@
 //      Build Number
 //In order to begin building localizable applications, set
 
-[assembly: AssemblyVersion("1.0.0.0")]
+//In order to begin building localizable applications, set
 
 
 
@@ -44,12 +44,12 @@
 //In order to begin building localizable applications, set
 
 //[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
-[assembly: AssemblyVersion("1.0.0.0")]
-
-//[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
-
-[assembly: AssemblyVersion("1.0.0.0")]
 [assembly: ThemeInfo(
+
+[assembly: AssemblyVersion("1.0.0.0")]
+//[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
 
 [assembly: AssemblyVersion("1.0.0.0")]
 //In order to begin building localizable applications, set
