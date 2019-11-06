56c699b85a028765a6904dd60de9d23dbd06ab69
Jalil <jalilmarkel1992@gmail.com>
2019-11-05 20:00:11 -0500

    useState related to MainWindow

--- WPF/MainWindow.xaml.cs
+++ WPF/MainWindow.xaml.cs
@@ -1,14 +1,14 @@
 using System.Text;
-        public MainWindow()
+using System.Windows.Data;
+using System.Text;
 using System.Text;
 using System.Windows.Data;
 using System.Windows.Data;
 using System.Text;
-using System.Windows.Data;
         public MainWindow()
         public MainWindow()
 using System.Windows.Data;
-        public MainWindow()
+using System.Windows.Data;
 using System.Windows.Data;
 using System.Windows.Data;
 using System.Text;
@@ -16,10 +16,10 @@ using System.Text;
 using System.Text;
 using System.Text;
         public MainWindow()
-using System.Text;
+using System.Windows.Data;
+using System.Windows.Data;
 using System.Windows.Data;
         public MainWindow()
-using System.Text;
 using System.Windows.Data;
 using System.Windows.Data;
 using System.Windows.Data;
