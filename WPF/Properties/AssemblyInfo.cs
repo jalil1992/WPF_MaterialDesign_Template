b28cea83cd3886a66cc73c205611dfba1ee5180e
Jalil <jalilmarkel1992@gmail.com>
2019-11-18 15:02:13 -0500

    User manage  AssemblyInfo

--- WPF/Properties/AssemblyInfo.cs
+++ WPF/Properties/AssemblyInfo.cs
@@ -4,52 +4,52 @@
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: ThemeInfo(
 //[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
+//[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
+//[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
+
+//[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
+//[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
+
+[assembly: ThemeInfo(
+[assembly: AssemblyVersion("1.0.0.0")]
+//[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
+
+//[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
+[assembly: AssemblyVersion("1.0.0.0")]
 [assembly: ThemeInfo(
 
-
 //[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
-//[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
-
-//[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
-[assembly: AssemblyVersion("1.0.0.0")]
-//[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
-    ResourceDictionaryLocation.SourceAssembly //where the generic resource dictionary is located
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: ThemeInfo(
-
-//[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
-
 
     ResourceDictionaryLocation.SourceAssembly //where the generic resource dictionary is located
+//      Build Number
 [assembly: AssemblyVersion("1.0.0.0")]
 //[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
 //      Build Number
 
-
+    ResourceDictionaryLocation.SourceAssembly //where the generic resource dictionary is located
 //In order to begin building localizable applications, set
-[assembly: AssemblyVersion("1.0.0.0")]
-
+//In order to begin building localizable applications, set
+[assembly: ThemeInfo(
     ResourceDictionaryLocation.SourceAssembly //where the generic resource dictionary is located
 
 
     ResourceDictionaryLocation.SourceAssembly //where the generic resource dictionary is located
-[assembly: ThemeInfo(
 //[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
 //[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
+//In order to begin building localizable applications, set
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: ThemeInfo(
 [assembly: ThemeInfo(
 
 //In order to begin building localizable applications, set
 
-[assembly: AssemblyVersion("1.0.0.0")]
+
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyVersion("1.0.0.0")]
 //[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
-[assembly: ThemeInfo(
 
-[assembly: AssemblyVersion("1.0.0.0")]
+
+//[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
 
 
 //In order to begin building localizable applications, set
