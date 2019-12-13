4de5b7fadb390484546c9e7f6aa989b4d6be47f6
Jalil <jalilmarkel1992@gmail.com>
2019-12-12 22:04:54 -0500

    Props change  AssemblyInfo

--- WPF/Properties/AssemblyInfo.cs
+++ WPF/Properties/AssemblyInfo.cs
@@ -1,55 +1,55 @@
-
-
-
 [assembly: AssemblyVersion("1.0.0.0")]
+
+
+//In order to begin building localizable applications, set
 //[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
 //[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
 //[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
+//[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
+//[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
+[assembly: ThemeInfo(
 //[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
 
+
+
 //[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
+
 //[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
 
 [assembly: ThemeInfo(
 
 //[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
-
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyVersion("1.0.0.0")]
 [assembly: ThemeInfo(
-
 //[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
-
     ResourceDictionaryLocation.SourceAssembly //where the generic resource dictionary is located
+[assembly: ThemeInfo(
+//[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
 //      Build Number
-//[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
-//[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
-//      Build Number
 
     ResourceDictionaryLocation.SourceAssembly //where the generic resource dictionary is located
 //In order to begin building localizable applications, set
-//In order to begin building localizable applications, set
+//[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
 [assembly: ThemeInfo(
+//[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
+
+
+[assembly: ThemeInfo(
+[assembly: AssemblyVersion("1.0.0.0")]
+//[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: ThemeInfo(
+
+
     ResourceDictionaryLocation.SourceAssembly //where the generic resource dictionary is located
 
 
+//In order to begin building localizable applications, set
+[assembly: AssemblyVersion("1.0.0.0")]
+//[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
+
+//[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
+//[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
+
     ResourceDictionaryLocation.SourceAssembly //where the generic resource dictionary is located
-//[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
-//[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
-//In order to begin building localizable applications, set
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: ThemeInfo(
-[assembly: ThemeInfo(
-
-//In order to begin building localizable applications, set
-
-
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyVersion("1.0.0.0")]
-//[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
-
-
-//[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
-
-
 //In order to begin building localizable applications, set
