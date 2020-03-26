b96494704b12ec543f47049d117affe05ab05f4e
Jalil <jalilmarkel1992@gmail.com>
2020-03-26 11:19:20 -0400

    User manage  AssemblyInfo

--- WPF/Properties/AssemblyInfo.cs
+++ WPF/Properties/AssemblyInfo.cs
@@ -15,7 +15,7 @@
 //[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
 
 //[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
-
+[assembly: ThemeInfo(
 [assembly: ThemeInfo(
 //[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
     ResourceDictionaryLocation.SourceAssembly //where the generic resource dictionary is located
@@ -30,7 +30,7 @@
 //In order to begin building localizable applications, set
 //[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
 [assembly: ThemeInfo(
-//[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
+[assembly: ThemeInfo(
 
 
 [assembly: ThemeInfo(
@@ -42,8 +42,8 @@
 //[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
 //      Build Number
 [assembly: ThemeInfo(
-//[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
-
+[assembly: ThemeInfo(
+[assembly: ThemeInfo(
 
 [assembly: AssemblyVersion("1.0.0.0")]
 //[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
