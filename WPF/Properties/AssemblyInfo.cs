4ca58a8c480f356e1cfc71e4a31291694a963be2
Jalil <jalilmarkel1992@gmail.com>
2020-04-09 22:17:49 -0400

    useState related to AssemblyInfo

--- WPF/Properties/AssemblyInfo.cs
+++ WPF/Properties/AssemblyInfo.cs
@@ -1,22 +1,22 @@
 [assembly: AssemblyVersion("1.0.0.0")]
-
+//[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
 
 [assembly: AssemblyVersion("1.0.0.0")]
 //[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
 [assembly: AssemblyVersion("1.0.0.0")]
 
-//[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
+[assembly: ThemeInfo(
 //[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
 [assembly: ThemeInfo(
 [assembly: AssemblyVersion("1.0.0.0")]
     ResourceDictionaryLocation.SourceAssembly //where the generic resource dictionary is located
 [assembly: ThemeInfo(
-
+[assembly: ThemeInfo(
 //[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
 
 //[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
 [assembly: ThemeInfo(
-[assembly: ThemeInfo(
+
 //[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
     ResourceDictionaryLocation.SourceAssembly //where the generic resource dictionary is located
 [assembly: ThemeInfo(
@@ -26,12 +26,12 @@
 //[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
 //      Build Number
 
-//[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
+
 //In order to begin building localizable applications, set
 //[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
 [assembly: ThemeInfo(
 [assembly: ThemeInfo(
-
+//[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
 
 [assembly: ThemeInfo(
 [assembly: AssemblyVersion("1.0.0.0")]
@@ -40,13 +40,13 @@
 [assembly: AssemblyVersion("1.0.0.0")]
 
 //[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
-//      Build Number
+//[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
 [assembly: ThemeInfo(
 [assembly: ThemeInfo(
 [assembly: ThemeInfo(
 
-[assembly: AssemblyVersion("1.0.0.0")]
-//[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
+    ResourceDictionaryLocation.SourceAssembly //where the generic resource dictionary is located
+
 
 //[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
 [assembly: ThemeInfo(
